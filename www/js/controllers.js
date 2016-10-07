2ac30237691d44a35fdb38008aa8ee6479f56696
issa-sliit <uaarangala@gmail.com>
2016-10-07 22:26:27 +0530

    chartafter

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -180,22 +180,7 @@ angular.module('app.controllers', [])
   $scope.data = [
     [65, 59, 80, 81, 56, 55, 40,28, 48, 40, 19, 86, 27]
   ];
-  $scope.onClick = function (points, evt) {
-    console.log(points, evt);
-  };
-  $scope.datasetOverride = [{ yAxisID: 'y-axis-1' }];
-  $scope.options = {
-    scales: {
-      yAxes: [
-        {
-          id: 'y-axis-1',
-          type: 'linear',
-          display: true,
-          position: 'left'
-        }
-      ]
-    }
-  };
+
   
    MonthPicker.init({});
         $scope.buttonTap = function() {
