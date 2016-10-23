13921a34715ebb1ff3ebdcef8311c4fe1d26cec2
branavi-vigneswaran <branavi-vigneswaran>
2016-10-23 04:59:40 -0700

    new again

--- www/js/services.js
+++ www/js/services.js
@@ -3,6 +3,37 @@
 
   angular
     .module('app.services', [])
+ function VechileFactory($firebaseArray,$q) {
+      var ref = new Firebase('https://snev.firebaseio.com/');
+      var parts = [];
+
+      return {
+        getVechilePartTypes: function() {
+          var defer = $q.defer();
+          ref.child('vechile_parts').on("value", function(snapshot) {
+            defer.resolve(snapshot.val());
+          });
+          return defer.promise;
+          //return $firebaseArray(ref.child('vechile_parts'));
+        },
+        getVechileParts: function(type) {
+          return $firebaseArray(ref.orderByChild('vechile_parts').child(type));
+        },
+        addVechileParts: function(part, type) {
+          ref.child('vechile_parts').child(type).push().set(part);
+          //return $firebaseArray(ref.child('vechile_parts').child(type));
+        },
+        addParts: function(part) {
+          parts.push(part);
+        },
+        removeParts: function(index) {
+          parts.splice(index, 1);
+        },
+        getParts: function() {
+          return parts;
+        }
+      }
+    }
 
     function RoomFactory($firebaseArray) {
         var ref = new Firebase('https://snev.firebaseio.com/');
@@ -43,7 +74,7 @@
       angular
         .module('app')
         .factory('RoomFactory', ['$firebaseArray', RoomFactory])
-
+  .factory('VechileFactory', ['$firebaseArray','$q', VechileFactory])
 
 
 
