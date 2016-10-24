58e638abcf686c1924e757d08ecafff8cd536800
Asanka <asanka.jsndr@gmail.com>
2016-10-24 16:31:53 +0530

    moment error fix

--- www/js/messages/controllers.js
+++ www/js/messages/controllers.js
@@ -346,23 +346,23 @@ function getMockMessages() {
 
 
 
-// configure moment relative time
-moment.locale('en', {
-  relativeTime: {
-    future: "in %s",
-    past: "%s ago",
-    s: "%d sec",
-    m: "a minute",
-    mm: "%d minutes",
-    h: "an hour",
-    hh: "%d hours",
-    d: "a day",
-    dd: "%d days",
-    M: "a month",
-    MM: "%d months",
-    y: "a year",
-    yy: "%d years"
-  }
-});
+// // configure moment relative time
+// moment.locale('en', {
+//   relativeTime: {
+//     future: "in %s",
+//     past: "%s ago",
+//     s: "%d sec",
+//     m: "a minute",
+//     mm: "%d minutes",
+//     h: "an hour",
+//     hh: "%d hours",
+//     d: "a day",
+//     dd: "%d days",
+//     M: "a month",
+//     MM: "%d months",
+//     y: "a year",
+//     yy: "%d years"
+//   }
+// });
 
   
